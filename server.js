35534cc550bd3534090b29b664c2afcec3587585
xuqingxin001 <33855438+xuqingxin001@users.noreply.github.com>
2022-04-12 14:56:45 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -24,83 +24,64 @@ app.get("/zh", (req, res) => {
 });
 
 function replace(t, a, b) {
-  const reg = new RegExp(String.raw`(\s${a}: \").*?(\")`);
+  const reg = new RegExp(String.raw`(${a}: \").*?(\")`);
   return t.replace(reg, "$1" + b + "$2");
 }
-
-function replaceAll(item, p1) {
-  for (const [k, v] of Object.entries(item)) {
-    if (k === "roots") {
-      let roots = v.map((root) => {
-        return {
-          ...root,
-          protect_file_link: false,
-        };
-      });
-      p1 = p1.replace(
-        /roots: \[([\s\S]*)\]/,
-        '"roots": ' + JSON.stringify(roots, "", "\t")
-      );
-      continue;
-    }
-    if (v === true) {
-      p1 = p1.replace(`${k}: false`, `${k}: true`);
-    } else if (v === false) {
-      p1 = p1.replace(`${k}: true`, `${k}: false`);
-    } else if (v instanceof Object) {
-      p1 = replaceAll(v, p1);
-    } else {
-      p1 = replace(p1, k, v);
-    }
-  }
-  return p1;
-}
-
 app.post("/getCode", async (req, res) => {
   const p = req.body;
   const r = await xf
     .post("https://www.googleapis.com/oauth2/v4/token", {
       urlencoded: {
-        code: p.indexConfig.authCode,
+        code: p.authCode,
         client_id: "202264815644.apps.googleusercontent.com",
         client_secret: "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
         redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
-        grant_type: "authorization_code",
-      },
+        grant_type: "authorization_code"
+      }
     })
     .json()
-    .catch((e) => null);
+    .catch(e => null);
   if (r === null) {
     return res.status(400).send({
       status: "fail",
       content: "",
       message:
-        "Authorization Code is invalid. Perhaps it doesn's exists or it has been used for 1 time.",
+        "Authorization Code is invalid. Perhaps it doesn's exists or it has been used for 1 time."
     });
   }
-//   https://raw.githubusercontent.com/Aicirou/goindex-theme-acrou/v${p.options.version}/go2index/index.js
   let code = await xf
-    .get(
-      'https://raw.githubusercontent.com/alx-xlx/goindex/master/goindex-acrou/go2index/index.js'
-      // `https://raw.githubusercontent.com/alx-xlx/goindex/31fabf3d7ff221ff34aaaf23e63a1a5a49e40b4f/goindex-acrou/go2index/index.js`
-    )
+    .get("https://raw.githubusercontent.com/alx-xlx/goindex/2.0.5-darkmode-0.1/goindex-acrou/go2index/index.js")
     .text();
   let options = code.match(
     /\/\/ =======Options START=======([\s\S]*)\/\/ =======Options END=======/
   )[0];
   code = code.replace(options, "");
   options = replace(options, "refresh_token", r.refresh_token);
-  options = options.replace(/var themeOptions = \{([\s\S]*)\}/, (p1) => {
-    return replaceAll(p.options, p1);
-  });
-  options = options.replace(/var authConfig = \{([\s\S]*)\}/, (p1) => {
-    return replaceAll(p.indexConfig, p1);
-  });
+  for (const [k, v] of Object.entries(p)) {
+    if (v === true) {
+      options = options.replace(`${k}: false`, `${k}: true`);
+    } else if (v === true) {
+      options = options.replace(`${k}: true`, `${k}: false`);
+    } else if (v instanceof Array) {
+      let roots = v.map(item => {
+        return {
+          ...item,
+          protect_file_link: false
+        };
+      });
+      options = options.replace(
+        /roots: \[([\s\S]*)\]/,
+        '"roots": ' + JSON.stringify(roots, "", "\t")
+      );
+    } else {
+      options = replace(options, k, v);
+    }
+  }
   res.set("Content-Type", "text/javascript; charset=utf-8");
   res.send({
     status: "success",
     content: options + code,
-    message: "",
+    message: ""
   });
 });
 
